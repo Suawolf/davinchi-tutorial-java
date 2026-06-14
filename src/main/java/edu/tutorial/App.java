c008189d8024aca88066e035f2a2021381e56853
Suawolf <lcgswolf@gmail.com>
2026-06-14 16:01:24 -0600

    Finished Video 3
    Signed-off-by: Suawolf <lcgswolf@gmail.com>

--- src/main/java/edu/tutorial/App.java
+++ src/main/java/edu/tutorial/App.java
@@ -1,13 +1,23 @@
 package edu.tutorial;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+//        System.out.println("Hello World!");
+        String clave = System.getenv().get("clave");
+        System.out.println("clave: " + clave);
+
+//        int value = System.in.read();
+
+        Scanner sc = new Scanner(System.in);
+        String value = sc.nextLine();
+
+        System.out.println("Value".concat(value));
     }
 }
